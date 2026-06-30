6352af60a596e4ac7642577a08caba512296a804
Guilherme Leme <leme.guilherme.p@gmail.com>
2026-06-30 10:42:02 -0300

    [post-release-ci] change tests files so it does not use the bbb customReporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: CI,
   reporter: CI
-    ? [['blob'], ['./core/setup/customReporter.ts']]
+    ? [['list'], ['blob']]
     : [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: server,
